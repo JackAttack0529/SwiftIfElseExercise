e804ef25c13af5220e2fb8f9e03446e9668a46e1
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-02-27 09:06:49 -0600

    Initial Commit

---
+++ SwiftIfElseExercise.playground/Contents.swift
@@ -0,0 +1,51 @@
+let turtle = ""
+
+if (turtle == "Leonardo") {
+    print("Katana")
+} else if (turtle == "Donatello"){
+print("Bo Staff")
+} else if (turtle == "Raphael") {
+    print("Twin Sai")
+} else if (turtle == "Michelangelo") {
+    print("Dual Nunchaku")
+} else {
+    print("Heros in a half shell. Turtle Power!")
+}
+
+
+/*1a. Create a let variable called turtle and set the value to an empty string
+1b. Create an If Else statement that includes IF, ELSE IFs, and ELSE statements. Set it up the following way...
+
+If the value of the turtle variable is "Leonardo" then have "Katana" display in the debug console when the file is run.
+
+If the value of the turtle variable is "Donatello" then have "Bo Staff" display in the debug console when the file is run.
+
+If the value of the turtle variable is "Raphael" then have "Twin Sai" display in the debug console when the file is run.
+
+If the value of the turtle variable is "Michelangelo" then have "Dual Nunchaku" display in the debug console when the file is run.
+
+If the value of the turtle variable is anything other than Leonardo, Donatello, Raphael, or Michelangelo then have "Heros in a half shell. Turtle Power!" display in the debug console when the file is run.*/
+
+/*
+Syntax Example
+ 
+ Switch value {
+ case value:
+    CODE TO BE RUN GOES HERE
+ case value:
+    CODE TO BE RUN GOES HERE
+ default:
+    CODE TO BE RUN GOES HERE
+ */
+
+
+let turtleVillains = ""
+
+switch turtleVillains {
+case "Shredder":
+    print("Tonight I dine on turtle soup.")
+case "Krang":
+    print("I HATE turtles, I despise turtles!")
+default:
+    print("Oh no! It must be the Foot Clan")
+}
